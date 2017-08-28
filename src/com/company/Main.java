ae340fc5c63a84b2750076f16b795ba71d84ec14
chainley56 <31398635+chainley56@users.noreply.github.com>
2017-08-28 05:33:03 -0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,56 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	
+int num = 0;
+
+
+
+
+	int count = 1;
+
+
+	 for (int i = 1; i<101; i++){
+
+
+		 num = i;
+
+
+		 while(num != 1){
+
+
+			 if(num%2 == 0){
+
+
+				 num = num / 2;
+
+
+			 }
+
+
+			 else{
+
+
+				 num = num * 3 + 1;
+
+
+			 }
+
+
+			 count++;
+
+
+		 }
+
+
+		 System.out.println(i + "-" + count);
+
+
+		 count = 1;
+
+
+	 }
+
+
     }
 }
